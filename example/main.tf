5088ab671faa50344f34046dbf4e00216519f4e3
narenderttn <narender.panwar@tothenew.com>
2023-09-25 17:25:47 +0530

    changes

--- example/main.tf
+++ example/main.tf
@@ -5,7 +5,7 @@ module "terraform-aws-elasticache" {
   engine             = "redis"
   clusters           = "2"
   failover           = "true"
-  subnets            = ["subnet-043d59b3957d49e1d", "subnet-093641ce3f549831e", "vpc-0c7ca42512bbbb3df"]
+  subnets            = ["subnet-043d59b3957d49e1d", "subnet-093641ce3f549831e", "subnet-0d911d25c86c0a429"]
   vpc_id             = "vpc-0c7ca42512bbbb3df"
   availability_zones = ["us-east-1a", "us-east-1b"]
   node_type          = "cache.r4.large"
