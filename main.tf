5e91aea28838fcebe550ae2b14ff341d55c2f9d2
narenderttn <narender.panwar@tothenew.com>
2023-09-25 17:20:11 +0530

    changes

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_elasticache_replication_group" "redis" {
   node_type                     = var.node_type
   automatic_failover_enabled    = var.failover
   #auto_minor_version_upgrade    = var.auto_minor_version_upgrade
-  availability_zones            = var.availability_zones
+  #availability_zones            = var.availability_zones
   multi_az_enabled              = var.multi_az_enabled
   engine                        = var.engine
   at_rest_encryption_enabled    = var.at_rest_encryption_enabled
@@ -33,7 +33,7 @@ resource "aws_elasticache_replication_group" "redis" {
   port                          = var.port
   parameter_group_name          = var.parameter_group_name
   subnet_group_name             = aws_elasticache_subnet_group.redis_subnet_group.id
-  security_group_names          = var.security_group_names
+  #security_group_names          = var.security_group_names
   security_group_ids            = [aws_security_group.redis_security_group.id]
   snapshot_arns                 = var.snapshot_arns
   snapshot_name                 = var.snapshot_name
