08bca639bf8b08640af5249ea93c1e3b541c508a
narenderttn <narender.panwar@tothenew.com>
2023-09-27 10:58:09 +0530

    changes

--- variable.tf
+++ variable.tf
@@ -45,7 +45,7 @@ variable "clusters" {
 variable "cluster_mode_enabled" {
   type        = bool
   description = "Flag to enable/disable creation of a native redis cluster. `automatic_failover_enabled` must be set to `true`. Only 1 `cluster_mode` block is allowed"
-  default     = false
+  default     = true
 }
 
 variable "failover" {
